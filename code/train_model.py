df4227e60614dce9e7761dbd3b4abeb3614d8f17
VortexExpansion <sen.2@iitj.ac.in>
2020-11-23 10:26:24 +0530

    Updated model to write predictions to file

--- code/train_model.py
+++ code/train_model.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
-
+import csv
 
 train = pd.read_csv("./data/train.csv")
+test = pd.read_csv("./data/test.csv")
 exposure = train.head()
 
-# for c in train.columns:
-#     print(c,100*train[c].isnull().sum()/len(train))
-#     To locate the missing values in the data table as percentages
+# for c in test.columns:
+#     print(c,100*test[c].isnull().sum()/len(test))
+    #To locate the missing values in the data table as percentages
 
 train['Age'] = train['Age'].fillna(train['Age'].mean()) #replacing null values by mean ages
-
+test['Age'] = test['Age'].fillna(test['Age'].mean())
+test['Fare'] = test["Fare"].fillna(test['Age'].mean())
 #print(exposure)
+
 
 dependencies_sex = train[['Sex', 'Survived']].groupby(['Sex'],as_index=False).mean()
 dependencies_Pclass = train[['Pclass', 'Survived']].groupby(['Pclass'],as_index=False).mean()
@@ -23,10 +26,12 @@ dependencies_parch = train[['Parch', 'Survived']].groupby(['Parch'],as_index=Fal
 # print(dependencies_parch) #parch 1,2,3 more likely
 
 train=train.drop(['PassengerId','Name','Cabin','Embarked','Ticket'],axis=1) #removing all redundant columns
+test=test.drop(['PassengerId','Name','Cabin','Embarked','Ticket'],axis=1)
 #print(train.head())
 
 Sex_mapping={'female':1,'male':0}
 train.Sex=[Sex_mapping[gender] for gender in train.Sex] #Replacing string feature by int for model fitting
+test.Sex=[Sex_mapping[gender] for gender in test.Sex]
 
 #print(train.head())
 
@@ -35,17 +40,25 @@ train.Sex=[Sex_mapping[gender] for gender in train.Sex] #Replacing string featur
 X=train.drop(['Survived'],axis=1)
 y=train.Survived
 #print(X.head())
-
+#print(test.head())
 # Decision Tree Classifier
 
 model_1=DecisionTreeClassifier(random_state=1)
 model_1.fit(X,y)
-prediction = model_1.predict(X)
-#print(prediction)
-percent_1=metrics.accuracy_score(prediction,y)
-print('Model accuracy of Decision Tree Classifier = {}%'.format(100*percent_1))
+prediction = model_1.predict(test)
+print(prediction)
 
+#percent_1=metrics.accuracy_score(prediction,y)
+#print('Model accuracy of Decision Tree Classifier = {}%'.format(100*percent_1))
 
+# Writing to a file
 
-
+with open('DTClassifier.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["PassengerId","Survived"])
+    ID=892
+    for loop_var in range(len(prediction)):
+        writer.writerow([ID,prediction[loop_var]])
+        ID+=1
+    
 
