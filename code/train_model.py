d489cdbf0fe1c0ec7f88751b737e574174197f8b
VortexExpansion <sen.2@iitj.ac.in>
2020-11-23 14:50:42 +0530

    Functionalised code and added docstrings

--- code/train_model.py
+++ code/train_model.py
@@ -3,14 +3,33 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 import csv
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 
-train = pd.read_csv("./data/train.csv")
-test = pd.read_csv("./data/test.csv")
-exposure = train.head()
+def test_accuracy(prediction,y):
+
+    '''
+    Func to check the fitting of model on the test data set
+    Parameters :
+    prediction = values of the output that are predicted by the model
+    y = actual values stated in test data  
+    '''
+
+    percent = metrics.accuracy_score(prediction,y)
+    print('Accuracy of the chosen model = {}%'.format(100*percent))
+
+
 
 def write_to_new_file(filename):
-    with open(filename, 'w', newline='') as file:
+
+    '''
+    Creates a new csv file in the output folder and writes the predictions of the model
+    Parameter: 
+    filename : name of the new csv file to be created 
+    ''' 
+
+    path="./output/"+filename
+    with open(path, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["PassengerId","Survived"])
         ID=892
@@ -18,9 +37,36 @@ def write_to_new_file(filename):
             writer.writerow([ID,prediction[loop_var]])
             ID+=1
 
-# for c in test.columns:
-#     print(c,100*test[c].isnull().sum()/len(test))
-    #To locate the missing values in the data table as percentages
+
+
+def train_model(model,filename='example.csv'):
+
+    '''
+    Func to train the model on the given test data set. 
+    Parameters : 
+    model : name of the model to be applied
+    filename : string which stores the name of newfile to be created. Deafault argument = test.csv
+    '''
+
+    model.fit(X,y)
+    global prediction
+    prediction = model.predict(test)
+    print(prediction)
+    write_to_new_file(filename)
+
+def check_missing_values(test):
+
+    '''
+    Func to print the percentage of missing values in the dataset
+    Parameter :
+    test : Given dataset
+    '''
+    for c in test.columns:
+        print(c,100*test[c].isnull().sum()/len(test))
+
+train = pd.read_csv("./data/train.csv")
+test = pd.read_csv("./data/test.csv")
+exposure = train.head()
 
 train['Age'] = train['Age'].fillna(train['Age'].mean()) #replacing null values by mean ages
 test['Age'] = test['Age'].fillna(test['Age'].mean())
@@ -54,30 +100,21 @@ y=train.Survived
 #print(test.head())
 
 
-'''
 
-# ----------------    Decision Tree Classifier   : This model got an accuracy of 73% on kaggle
+'''
+# ----------------    Decision Tree Classifier   : This model got an accuracy of 73.6% on kaggle
 
 model_1=DecisionTreeClassifier(random_state=1)
-model_1.fit(X,y)
-prediction = model_1.predict(test)
-print(prediction)
+train_model(model_1,'test1.csv')
 
-#percent_1=metrics.accuracy_score(prediction,y)
-#print('Model accuracy of Decision Tree Classifier = {}%'.format(100*percent_1))
+# ----------------- Random Forest Classifier : This model got an accuracy of 74.4% on kaggle
 
-write_to_new_file('DTClassifier.csv')
-  
+model_2 = RandomForestClassifier(random_state=2)
+train_model(model_2,'test3.csv')
 
-'''
+# ----------------- Logistic Regression : This model got an accuracy of 75.8% on kaggle
 
-'''
-# ----------------- Random Forest Classifier : This model got an accuracy of 74% on kaggle
+model_3 = LogisticRegression(random_state=3)
+train_model(model_3,'LogisticRegression.csv')
 
-model_2 = RandomForestClassifier(random_state=1)
-model_2.fit(X,y)
-prediction = model_2.predict(test)
-print(prediction)
-
-write_to_new_file('RandomForestClassifier.csv')
 '''
