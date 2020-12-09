3281a2bbe8bd38b18b8349e85fabade03ca5ee03
VortexExpansion <sen.2@iitj.ac.in>
2020-12-09 20:38:04 +0530

    add plot

--- code/plot.py
+++ code/plot.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 from train_model import get_train
 
+import seaborn as sns
+sns.set(font_scale=1)
+
 train_data = get_train()
 print(train_data.head())
 
@@ -19,3 +22,18 @@ plt.show()
 print(train_data['Sex'].value_counts().plot(kind="bar"))
 plt.title('Sex')
 plt.show()
+
+
+# Optimization, more visual, use this later
+
+# g = sns.catplot(x="Sex", y="Survived", col="Pclass",
+#                     data=train_data, saturation=.5,
+#                     kind="bar", ci=None, aspect=.6)
+# (g.set_axis_labels("", "Survival Rate")
+#     .set_xticklabels(["Men", "Women"])
+#     .set_titles("{col_name} {col_var}")
+#     .set(ylim=(0, 1))
+#     .despine(left=True))  
+# plt.subplots_adjust(top=0.8)
+# g.fig.suptitle('Survival according to passenger class')
+# plt.show()
